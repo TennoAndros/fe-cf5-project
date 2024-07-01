283f55ee7ed400efe912f3294ee5f8e51913547b
Andreas <andreasjr77@outlook.com>
2024-07-01 21:41:43 +0300

    update for host

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:9090";
+export const API_BASE_URL = "https://be-cf5-project.onrender.com";
 
 // "http://localhost:9090";
 // "https://be-cf5-project.onrender.com"
